f700cea5bd35955c14047272dc5c2310a1f1d3b9
Edwin Koch <eddyed.k@gmail.com>
2020-07-08 18:01:36 +0200

    started work on bash file

---
+++ setup_ekdev.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#blabla 
+#fzdjhgfklkjöL
+
+$packageManger="apt-get"
+
+echo "Wellcome to my configuration file"
+
+read -p "do you want to continue (y/n) ?: " yesorno
+
+if [ "$yesorno" = "y" ];
+then
+
+    echo "updating system..."
+    sudo $packageManger update
+    sudo $packageManger upgrade
+    echo "...done!"
+
+
+    echo "installing vim...."
+    sudo $packageManger install -y vim 
+    echo "...Done installin vim"
+
+    sudo $packageManger install -y mc
+    sudo $packageManger install -y htop
+
+    sudo $packageManger install -y gcc 
+    sudo $packageManger install -y cmake
+    sudo $packageManger install -y i2ctools
+    sudo $packageManger install -y gdb
+
+else 
+    echo "Terminating"
+fi
+
+
+
